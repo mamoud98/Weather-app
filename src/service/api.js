9723c139a7df8d9aaa94cd9a55f8a2bd6bf9828b
Mahmoud Shorafa <m2shorafa@gmail.com>
2021-11-01 14:14:24 +0200

    add https

--- src/service/api.js
+++ src/service/api.js
@@ -1,10 +1,10 @@
 const getDaysWeather = (CityName, lang, units) => {
-  const url = `http://api.openweathermap.org/data/2.5/forecast?q=${CityName}&lang=${lang}&units=${units}&appid=2298ad5da860eaa72f49a587eed1dce8`;
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${CityName}&lang=${lang}&units=${units}&appid=2298ad5da860eaa72f49a587eed1dce8`;
   return fetch(url);
 };
 
 const getCurrentWeather = (CityName, lang = "en") => {
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${CityName}&lang=${lang}&units=metric&appid=2298ad5da860eaa72f49a587eed1dce8`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${CityName}&lang=${lang}&units=metric&appid=2298ad5da860eaa72f49a587eed1dce8`;
   return fetch(url);
 };
 
